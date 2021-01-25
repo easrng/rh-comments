aa7a5b58e1d81fad3136384b07706b182c64af63
easrng <easrng@gmail.com>
2021-01-25 15:05:52 -0500

    Update Comment.svelte

--- src/Comment.svelte
+++ src/Comment.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <div class="comment">
-  <p>{comment.body}</p>
+  <div class="userinfo">
+    <a href={"https://routinehub.co/user/"+encodeURIComponent(comment.user)} class="user">@{comment.user}</a>
+    {#each comment.tags as tag}
+      <span class={"tag tag-"+tag.toLowerCase()}>{tag}</span>
+    {/each}
+    <span class="date">{comment.date}</span>
+  </div>
+  <div class="body">{comment.body}</div>
   {#if comment.children.length>0}
     <div class="replies">
       {#each comment.children as reply}
