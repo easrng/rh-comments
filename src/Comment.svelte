9bcfeedba2bf9a4aad17dd9c08eb726baf583bf5
easrng <easrng@gmail.com>
2021-01-25 14:32:28 -0500

    Update Comment.svelte

--- src/Comment.svelte
+++ src/Comment.svelte
@@ -4,11 +4,11 @@
 
 <div class="comment">
   <p>{comment.body}</p>
-  {#if comment.children.length>1}
+  {#if comment.children.length>0}
     <div class="replies">
       {#each comment.children as reply}
         <svelte:self comment={reply} />
-    	{/each}
+      {/each}
     </div>
   {/if}
 </div>
