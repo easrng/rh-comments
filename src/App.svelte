81ef28dadf21871b7b81660079d1d9f0ba127269
easrng <easrng@gmail.com>
2021-01-25 14:23:35 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,24 @@
 <script>
   import { onMount } from "svelte";
-  export let date;
+  import Comment from './Comment.svelte';
+  export let comments;
 
   onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
+    const res = await fetch("/api/comments.js?id=6565");
+    const newComments = await res.json();
+    comments = newComments;
   });
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <h1>Comments</h1>
+  <div class="comments">
+    {#if !comments}
+      Loading comments...
+    {:else}
+      {#each comments as comment}
+        <Comment {comment} />
+    	{/each}
+    {/if}
+  </div>
 </main>
