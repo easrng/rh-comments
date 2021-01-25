d3df1f34032c81ea23fea7e6ba7bae3aa3d04929
easrng <easrng@gmail.com>
2021-01-25 14:28:37 -0500

    Create Comment.svelte

---
+++ src/Comment.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Comment from './Comment.svelte';
+  export let comment;
+</script>
+
+<div class="comment">
+  <p>{comment.body}</p>
+  {#if comment.children.length>1}
+    <div class="replies">
+      {#each comment.children as reply}
+        <:Self comment={reply} />
+    	{/each}
+    </div>
+  {/if}
+</div>
